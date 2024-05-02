33acb10ad57347a3664d2a18173f665bbdbb21b2
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-05-02 19:43:46 +0300

    fix: remove back button exit animation

--- src/features/navigate-to-back/ui/back-button.tsx
+++ src/features/navigate-to-back/ui/back-button.tsx
@@ -2,7 +2,7 @@
 
 'use client';
 
-import { AnimatePresence, m } from 'framer-motion';
+import { m } from 'framer-motion';
 import { usePathname, useRouter } from 'next/navigation';
 import { useCallback } from 'react';
 import { PiCaretLeftBold } from 'react-icons/pi';
@@ -23,25 +23,23 @@ export const NavigateBackButton = () => {
   }, [isWithinPage, router]);
 
   return (
-    <AnimatePresence>
-      {isBackableRoute && (
-        <m.button
-          initial={{ opacity: 0, filter: 'blur(24px)' }}
-          animate={{ opacity: 1, filter: 'blur(0px)' }}
-          exit={{
-            opacity: 0,
-            filter: 'blur(24px)',
-            transition: { duration: 0.3 },
-          }}
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.95 }}
-          className='backdrop-blur-2xl bg-default/70 shadow-base fixed flex gap-2 items-center top-4 left-4 xl:top-10 xl:left-10 rounded-full pl-2 pr-4 py-1 z-40 font-medium text-md md:text-lg'
-          onClick={handleClick}
-        >
-          <PiCaretLeftBold className='w-auto h-1/2' />
-          Назад
-        </m.button>
-      )}
-    </AnimatePresence>
+    isBackableRoute && (
+      <m.button
+        initial={{ opacity: 0, filter: 'blur(24px)' }}
+        animate={{ opacity: 1, filter: 'blur(0px)' }}
+        exit={{
+          opacity: 0,
+          filter: 'blur(24px)',
+          transition: { duration: 0.3 },
+        }}
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+        className='backdrop-blur-2xl bg-default/70 shadow-base fixed flex gap-2 items-center top-4 left-4 xl:top-10 xl:left-10 rounded-full pl-2 pr-4 py-1 z-40 font-medium text-md md:text-lg'
+        onClick={handleClick}
+      >
+        <PiCaretLeftBold className='w-auto h-1/2' />
+        Назад
+      </m.button>
+    )
   );
 };
