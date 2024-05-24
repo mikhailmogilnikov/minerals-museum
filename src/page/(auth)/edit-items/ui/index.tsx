9ac44111a7eaadf4de9fcee932d7deea9ab6f071
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-05-24 10:13:12 +0300

    fix: validate token

--- src/page/(auth)/edit-items/ui/index.tsx
+++ src/page/(auth)/edit-items/ui/index.tsx
@@ -2,12 +2,21 @@
 
 import { Button } from '@nextui-org/button';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import { PiPlusCircleBold } from 'react-icons/pi';
 import { useItems } from '@/shared/lib/providers/items-provider';
+import { MineralType } from '@/shared/model/mineral.type';
 import { Card } from '@/shared/ui/card';
 
 export const EditItemsPage = () => {
   const items = useItems();
+  const [itemsList, setItemsList] = useState<MineralType[]>([]);
+
+  useEffect(() => {
+    if (items.length > 0) {
+      setItemsList(items.reverse());
+    }
+  }, [items]);
 
   return (
     <div className='w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-4 md:gap-8'>
@@ -18,7 +27,7 @@ export const EditItemsPage = () => {
       >
         <PiPlusCircleBold size={150} className='opacity-50 flex-shrink-0 ' />
       </Button>
-      {items.map(({ name, id, img }) => (
+      {itemsList.map(({ name, id, img }) => (
         <div key={id} className='w-full aspect-[3/4]'>
           <Card title={name} img={img} path={`/admin/edit/item/${id}`} />
         </div>
