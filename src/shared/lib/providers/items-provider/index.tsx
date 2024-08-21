128f6ca6a2cee898177a0f2f542d024f43be082a
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-08-21 17:22:08 +0300

    init dev

--- src/shared/lib/providers/items-provider/index.tsx
+++ src/shared/lib/providers/items-provider/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import axios from 'axios';
+
 import { createContext, useContext, useEffect, useState } from 'react';
-import { API_URL } from '@/shared/config';
+import axios from '@/shared/config/axios';
 import { MineralType } from '@/shared/model/mineral.type';
 
 const ItemsContext = createContext<MineralType[]>([]);
@@ -14,12 +14,7 @@ export const ItemsProvider = ({ children }: Props) => {
   const [list, setList] = useState<MineralType[]>([]);
 
   useEffect(() => {
-    const getItems = async () => {
-      const response = await axios.get<MineralType[]>(`${API_URL}/items/`);
-      setList(response.data);
-    };
-
-    getItems();
+    axios.get<MineralType[]>('items/').then((resp) => setList(resp.data));
   }, []);
 
   return <ItemsContext.Provider value={list}>{children}</ItemsContext.Provider>;
